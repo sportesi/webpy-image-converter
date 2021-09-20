b736f127e069d8e9e1bbd403f63cff7db6928d55
Sebastian Portesi <sebastian@wcanvas.com>
2021-09-20 16:26:14 -0300

    Changes to main function

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # How To: JPG, PNG to WEBP
 
 Para usar este `script` tenemos que hacer lo siguiente:
-1. Clonar el repositorio
+1. Clonar el repositorio dentro de la raiz del proyecto wordpress
 1. Asegurarnos de tener `python3` y `virtualenv` instalados
 1. Ejecutar el siguiente comando dentro del proyecto
     ```
     virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
     ```
-1. Correr el script (dentro del virtualenv) y apuntar al path absoluto de, por ejemplo, los uploads de un sitio wordpress. El script va a buscar todas las imagenes jpg o png y las convertira a webp.
+1. Correr el script (dentro del virtualenv). El script va a buscar todas las imagenes jpg o png dentro de la carpeta wp-content/uploads y las convertira a webp.
     ```
-    python3 main.py /absolute/dir/to/uploads
-    ```
-1. El script va a generar un archivo `.zip` en el destino que hayamos elegido
-    ```
-    Zip file generated in ./uploads.zip
+    python3 main.py
     ```
 1. Podemos salir del `virtualenv` con el comando `deactivate` o simplemente cerrando la consola
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from PIL import Image
 import os
-import sys
-import zipfile
 
 
 def main():
-    walk_dir = sys.argv[1]
-    zip_path = f"{walk_dir}.zip"
-    zip = zipfile.ZipFile(zip_path, 'w')
+    walk_dir = '../wp-content/uploads'
 
     for root, subdir, files in os.walk(walk_dir):
         ext = ('jpg', 'jpeg', 'png')
@@ -15,24 +11,16 @@ def main():
 
         for image in images:
             image_path = os.path.join(root, image)
-            # print(f"Converting {image_path} to WebP")
-            webp_path = converter(image_path=image_path)
-            # print(f"Image converted: {webp_path}")
-            zip.write(webp_path)
-            os.remove(webp_path)
-
-    zip.close()
-
-    print(f"Zip file generated in {zip_path}")
+            print(f"Converting {image_path}")
+            converter(image_path=image_path)
 
 
 def converter(image_path):
     with Image.open(image_path) as image:
         image.convert('RGBA')
         new_path = ''.join(image_path.split('.')[:-1])
-        new_path = os.path.join(new_path + '.webp')
+        new_path = f"..{new_path}.webp"
         image.save(new_path, 'webp')
-        return new_path
 
 
 if __name__ == '__main__':
